66e9837fb6381eb95c0c2e78072b96da36391308
zakattack9 <zakattack26111@gmail.com>
2020-11-21 22:14:27 -1000

    working on modal

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main extends JFrame {
     MenuBar menuBar = new MenuBar();
     this.setJMenuBar(menuBar.getMenuBar());
 
+    Modal addAttendanceModal = new Modal(this, "Add Attendance");
+    Modal loadRosterModal = new Modal(this, "Load a Roster");
+
     // configure JFrame window properties
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(1300, 800);
