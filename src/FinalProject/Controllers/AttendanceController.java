cb52bc11d5b87ff3cccce0bd743ece28ffffcfe2
zakattack9 <zakattack26111@gmail.com>
2020-11-24 13:20:22 -1000

    finished parser for attendance csv files

--- src/FinalProject/Controllers/AttendanceController.java
+++ src/FinalProject/Controllers/AttendanceController.java
@@ -3,6 +3,7 @@ package FinalProject.Controllers;
 import FinalProject.Components.InputModal;
 import FinalProject.Inputs.DateInput;
 import FinalProject.Inputs.FilePathInput;
+import FinalProject.Parsers.AttendanceParser;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -11,26 +12,35 @@ import java.util.Map;
 
 public class AttendanceController implements ActionListener {
   JFrame frame;
+  String filePathInputLabel;
+  String dateInputLabel;
 
   public AttendanceController(JFrame frame) {
     this.frame = frame;
+    this.filePathInputLabel = "Attendance File Path";
+    this.dateInputLabel = "Date";
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
     InputModal addAttendanceModal = createInputModal(frame);
     Map<String, String> inputs = addAttendanceModal.showModal();
-    System.out.println("CLICKED ADD ATTENDANCE");
+    if (inputs.size() > 0) parseCSVFile(inputs.get(filePathInputLabel), inputs.get(dateInputLabel));
+  }
+
+  private void parseCSVFile(String filePath, String date) {
+    AttendanceParser attendanceParser = new AttendanceParser(filePath, date);
+    attendanceParser.runParser();
   }
 
   private InputModal createInputModal(JFrame frame) {
     InputModal inputModal = new InputModal(frame, "Add Attendance");
 
     FilePathInput filePathInput = new FilePathInput(frame);
-    inputModal.addInput("Attendance File Path", filePathInput, filePathInput.getErrorMessage());
+    inputModal.addInput(this.filePathInputLabel, filePathInput, filePathInput.getErrorMessage());
 
     DateInput dateInput = new DateInput();
-    inputModal.addInput("Date", dateInput, dateInput.getErrorMessage());
+    inputModal.addInput(this.dateInputLabel, dateInput, dateInput.getErrorMessage());
     return inputModal;
   }
 }
---
+++ src/FinalProject/Parsers/AttendanceParser.java
@@ -0,0 +1,52 @@
+package FinalProject.Parsers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class AttendanceParser {
+  String filePath;
+  String date;
+
+  public AttendanceParser(String filePath, String date) {
+    this.filePath = filePath;
+    this.date = date;
+  }
+
+  public void runParser() {
+    Scanner parser = initializeParser();
+    while(parser.hasNextLine()) {
+      Scanner line = new Scanner(parser.nextLine());
+      line.useDelimiter(",");
+
+      String asurite = line.next();
+      int time = parseToInteger(line.next());
+      addToDatabse(asurite, date, time);
+    }
+  }
+
+  private void addToDatabse(String asurite, String date, int time) {
+    // access database through main
+  }
+
+  private int parseToInteger(String stringInt) {
+    int integer = 0;
+    try {
+      integer = Integer.parseInt(stringInt.trim());
+    } catch (NumberFormatException e) {
+      e.printStackTrace();
+    }
+    return integer;
+  }
+
+  private Scanner initializeParser() {
+    Scanner scanner = null;
+    try {
+      File file = new File(filePath);
+      scanner = new Scanner(file);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return scanner;
+  }
+}
