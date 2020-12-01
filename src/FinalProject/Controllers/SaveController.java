76be7be6f6bf4f5d58048db73f0c9811155df5d6
johnmawoods <johnmawoods@yahoo.com>
2020-11-30 22:56:30 -0700

    finished savefunctionality

--- src/FinalProject/Controllers/SaveController.java
+++ src/FinalProject/Controllers/SaveController.java
@@ -1,15 +1,27 @@
 package FinalProject.Controllers;
 
+import FinalProject.Models.Utilities.SaveFunctionality;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Handles actions and functionality when clicking "Save".
+ */
 public class SaveController implements ActionListener {
   public SaveController() {
 
   }
 
+  /***
+   * Called when "save" is selected by the user. Opens the save file dialog
+   * box which allows the user to specify a file name and directory then saves the data.
+   * @param e event passed in from the ActionListener.
+   */
   @Override
   public void actionPerformed(ActionEvent e) {
 
+    SaveFunctionality s = new SaveFunctionality();
+    s.saveCSV();
   }
 }
--- src/FinalProject/Models/Utilities/SaveFunctionality.java
+++ src/FinalProject/Models/Utilities/SaveFunctionality.java
@@ -1,29 +1,58 @@
 package FinalProject.Models.Utilities;
 
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 import javax.swing.*;
-import javax.swing.table.TableModel;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/***
+ * Saves all the data currently in the table.
+ */
 public class SaveFunctionality {
 
 
+    /***
+     * This function is called in the SaveController and implements the
+     * functionality of saving the data currently in the table.
+     */
+    public void saveCSV()
+    {
+        final JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fileChooser.setDialogTitle("Specify a file to save");
+        int value = fileChooser.showSaveDialog(null);
 
-    DatabaseConverter con = new DatabaseConverter();
-    String[][] data = con.getArrayModel();
-    FileWriter csv = new FileWriter(new File("save.csv"));
+        if(value == JFileChooser.APPROVE_OPTION)
+        {
+            try{
 
-    for(String[] row : data) {
-        String joinedString = String.join(",", row) + "\n";
-        csv.write(joinedString);
+                File file = fileChooser.getSelectedFile();
+                DatabaseConverter con = new DatabaseConverter();
+                String[][] data = con.getArrayModel();
+                FileWriter csv = new FileWriter(file);
+
+                // adds headers
+                String headers = String.join(",", con.getDBKeys()) + "\n";
+                csv.write(headers);
+
+                //adds data
+                for(String[] row : data) {
+                    String joinedString = String.join(",", row) + "\n";
+                    csv.write(joinedString);
+                }
+
+                csv.close();
+
+            }
+            // handles IOException
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+
+        }
+
+
     }
 
-    public SaveFunctionality() throws IOException {
-
-    }
 }
 
